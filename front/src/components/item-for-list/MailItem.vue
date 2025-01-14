efe4aef434e566874cc927dc4cad34659877ebbb
Evgeniy <bannih.j@yandex.ru>
2025-01-14 21:06:56 +0300

    добавлены стили в карточку письма, в шапке поиск перенесён в центр

--- front/src/components/item-for-list/MailItem.vue
+++ front/src/components/item-for-list/MailItem.vue
@@ -1,17 +1,19 @@
 <template lang="pug">
-q-card
-  q-card-section.row.items-center.q-pb-none.justify-between
-    div.text-h6.text-primary Информация о письме
-    q-btn(icon="close" flat round color="negative" @click="closeFn")
+  q-card
+    q-card-section.row.items-center.q-pb-none.justify-between
+      div.text-h6.col-10.text-primary Информация о письме
+      q-btn(icon="close" flat round color="negative" @click="closeFn")
 
-  q-card-section
-    div.text-h6 {{ !!data.theme ? data.theme : "" }}
-    div.text-subtitle2 {{ data.from }}
-    div.text-subtitle2 {{ data.date }}
+    q-card-section
+      div.text-h6 Тема: {{ !!data.theme ? data.theme : "" }}
+      .q-pa-md
+      div.text-subtitle2.text-data От: {{ data.from }}
+      div.text-subtitle2 {{ data.date }}
 
-  q-card-section
-    div.q-pa-sm {{ data.text }}
-</template>
+    q-card-section
+      div.q-pa-md.text-subtitle2.text-data
+        div.message-bubble Сообщение: {{ data.text }}
+  </template>
 
 <script setup>
 const props = defineProps({
@@ -27,3 +29,14 @@ const closeFn = () => {
   emit("close");
 };
 </script>
+<style scoped>
+.message-bubble {
+  display: inline-block;
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-color: #e0e0e0;
+  border: 1px solid #ccc;
+  max-width: 100%;
+  word-wrap: break-word;
+}
+</style>
--- front/src/layouts/MainLayout.vue
+++ front/src/layouts/MainLayout.vue
@@ -1,41 +1,40 @@
 <template lang="pug">
-q-layout(view="lHh Lpr lFf")
-  q-header(elevated)
-    q-toolbar
-      q-toolbar-title Моя Почта
+  q-layout(view="lHh Lpr lFf")
+    q-header(elevated)
+      q-toolbar.q-pa-md.row
+        q-toolbar-title.text-h4.title-style.col-4 Моя Почта
+        .q-pa-md
+        q-input.search-input.bg-white(
+          v-model="search"
+          outlined
+          rounded
+          label="Введите поисковое значение"
+          dense
+          style="width: 400px"
+        )
+        .q-pa-md.row.col-1
+        q-btn(
+          :loading="loading[3]"
+          color="primary"
+          @click="updateMails"
+          style="width: 150px; margin-left: 10px"
+        ) Обновить
+        .q-pa-md
+        q-btn(flat label="Отправить письмо" @click="openSendMailDialog")
+        .q-pa-md
+        q-btn(
+          color="negative"
+          label="Удалить выбранные"
+          @click="deleteSelectedMails"
+          :disabled="selectedMails.length === 0"
+        )
 
-      q-input(
-        v-model="search"
-        class="bg-white"
-        outlined
-        rounded
-        label="Введите поисковое значение"
-        dense
-        style="width: 400px"
-      )
+    q-dialog(v-model="sendMailDialogVisible")
+      send-mail-form(@close-form="sendMailDialogVisible = !sendMailDialogVisible")
 
-      q-btn(
-        :loading="loading[3]"
-        color="primary"
-        @click="updateMails"
-        style="width: 150px; margin-left: 10px"
-      ) Обновить
-
-      q-btn(flat label="Отправить письмо" @click="openSendMailDialog")
-
-      q-btn(
-        color="negative"
-        label="Удалить выбранные"
-        @click="deleteSelectedMails"
-        :disabled="selectedMails.length === 0"
-      )
-
-  q-dialog(v-model="sendMailDialogVisible")
-    send-mail-form(@close-form="sendMailDialogVisible = !sendMailDialogVisible")
-
-  q-page-container
-    router-view
-</template>
+    q-page-container
+      router-view
+  </template>
 
 <script setup>
 import { ref, watch, computed } from "vue";
@@ -73,3 +72,16 @@ watch(
   }
 );
 </script>
+<style scoped>
+.title-style {
+  color: white;
+  font-weight: bold;
+  text-shadow: hsl(from color h s l);
+  margin-left: 20px;
+}
+.search-input {
+  background: rgba(255, 255, 255, 0.9);
+  border-radius: 25px;
+  transition: all 0.3s ease;
+}
+</style>
